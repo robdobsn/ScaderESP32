31af5cda451298244d328aa607793c66eb95b9d8
Rob Dobson <robdobsn@users.noreply.github.com>
2024-02-04 11:25:42 +0000

    Moved common features to systypes/Common

---
+++ systypes/Common/features.cmake
@@ -0,0 +1,24 @@
+# Set the target Espressif chip
+set(IDF_TARGET "esp32")
+
+# System version
+add_compile_definitions(SYSTEM_VERSION="6.0.5")
+
+# Raft components
+set(RAFT_COMPONENTS
+    RaftSysMods@ReWorkConfigBase
+    RaftI2C@ReWorkConfigBase
+    RaftMotorControl@ReWorkConfigBase
+    RaftWebServer@ReWorkConfigBase
+)
+
+# File system
+set(FS_TYPE "littlefs")
+set(FS_IMAGE_PATH "../Common/FSImage")
+
+# Web UI
+set(UI_SOURCE_PATH "../Common/WebUI")
+# Uncomment the following line if you do not want to gzip the web UI
+# set(WEB_UI_GEN_FLAGS ${WEB_UI_GEN_FLAGS} --nogzip)
+# Uncomment the following line to include a source map for the web UI - this will increase the size of the web UI
+# set(WEB_UI_GEN_FLAGS ${WEB_UI_GEN_FLAGS} --incmap)
--- systypes/LightScader/features.cmake
+++ systypes/LightScader/features.cmake
@@ -1,24 +1,5 @@
-# Set the target Espressif chip
-set(IDF_TARGET "esp32")
-
-# System version
-add_compile_definitions(SYSTEM_VERSION="6.0.1")
+# Include common features
+include("${BUILD_CONFIG_DIR}/../Common/features.cmake")
 
 # Enable the ethernet hardware for Olimex ESP32 PoE-ISO boards
 add_compile_definitions(HW_ETH_PHY_LAN87XX)
-
-# Raft components
-set(RAFT_COMPONENTS
-    RaftSysMods@ReWorkConfigBase
-    RaftI2C@ReWorkConfigBase
-    RaftMotorControl@ReWorkConfigBase
-    RaftWebServer@ReWorkConfigBase
-)
-
-# File system
-set(FS_TYPE "littlefs")
-set(FS_IMAGE_PATH "../Common/FSImage")
-
-# Web UI
-set(UI_SOURCE_PATH "../Common/WebUI")
-# set(WEB_UI_GEN_FLAGS --nogzip)
